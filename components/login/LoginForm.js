4888d99d27615321f5980c45dd755726f55b50dd
Rokas <rookas.rudzenskas@gmail.com>
2021-11-30 15:31:22 +0200

    Rokas

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -14,42 +14,56 @@ const LoginForm = () => {
 
     return (
         <View style={styles.wrapper}>
-            <View style={styles.inputField}>
-                <TextInput
-                    placeholder="Phone number, username or email"
-                    placeholderTextColor={'#444'}
-                    autoCapitalize={'none'}
-                    autoCorrect={false}
-                    keyboardType="email-address"
-                    textContentType={'emailAddress'}
-                    autoFocus={true}
-                />
-            </View>
-                <View style={styles.inputField}>
-                    <TextInput
-                        placeholder="Password"
-                        placeholderTextColor={'#444'}
-                        autoCapitalize={'none'}
-                        autoCorrect={false}
-                        keyboardType="default"
-                        textContentType={'password'}
-                        autoFocus={false}
-                        secureTextEntry={true}
-                    />
-                </View>
-            <View style={{alignItems: 'flex-end', marginBottom: 30}}>
-                <Text style={{color: '#6BB0F5'}}>Forgot Password?</Text>
-            </View>
-            <TouchableOpacity activeOpacity={0.5} style={styles.button} onPress={() => console.log('Yoou have clicked me 👉 🔥')}>
-                <Text style={{fontWeight: '600', color: 'white'}}>Login</Text>
-            </TouchableOpacity>
+            <Formik
+                initialValues={{email: '', password: ''}}
+                onSubmit={(values, actions) => {
+                    console.log(values);
+                    // actions.setSubmitting(false);
+                }}
+                validationSchema={LoginFormSchema}
+                validateOnMount={true}
+            >
+                {({handleChange, handleSubmit, handleBlur, values, isValid}) => (
+              <>
+                  <View style={styles.inputField}>
+                      <TextInput
+                          placeholder="Phone number, username or email"
+                          placeholderTextColor={'#444'}
+                          autoCapitalize={'none'}
+                          autoCorrect={false}
+                          keyboardType="email-address"
+                          textContentType={'emailAddress'}
+                          autoFocus={true}
+                      />
+                  </View>
+                  <View style={styles.inputField}>
+                      <TextInput
+                          placeholder="Password"
+                          placeholderTextColor={'#444'}
+                          autoCapitalize={'none'}
+                          autoCorrect={false}
+                          keyboardType="default"
+                          textContentType={'password'}
+                          autoFocus={false}
+                          secureTextEntry={true}
+                      />
+                  </View>
+                  <View style={{alignItems: 'flex-end', marginBottom: 30}}>
+                      <Text style={{color: '#6BB0F5'}}>Forgot Password?</Text>
+                  </View>
+                  <TouchableOpacity activeOpacity={0.5} style={styles.button} onPress={() => console.log('Yoou have clicked me 👉 🔥')}>
+                      <Text style={{fontWeight: '600', color: 'white'}}>Login</Text>
+                  </TouchableOpacity>
 
-            <View style={{marginTop: 10,}}>
-                <Text style={{color: '#343434'}}>Don't have an account?</Text>
-                <TouchableOpacity activeOpacity={0.5}>
-                    <Text style={{color: '#6BB0F5'}}>Sign up</Text>
-                </TouchableOpacity>
-            </View>
+                  <View style={{marginTop: 10,}}>
+                      <Text style={{color: '#343434'}}>Don't have an account?</Text>
+                      <TouchableOpacity activeOpacity={0.5}>
+                          <Text style={{color: '#6BB0F5'}}>Sign up</Text>
+                      </TouchableOpacity>
+                  </View>
+              </>
+                    )}
+            </Formik>
         </View>
     );
 };
