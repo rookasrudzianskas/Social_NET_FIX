560c3f75827184cc0b6267c3448e2e758bde0fcc
Rokas <rookas.rudzenskas@gmail.com>
2021-11-30 12:42:10 +0200

    Rokas

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -4,14 +4,8 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "../screens/HomeScreen";
 
-function HomeScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
-    );
-}
 
 const Stack = createNativeStackNavigator();
 
