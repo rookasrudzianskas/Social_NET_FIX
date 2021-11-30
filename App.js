b4565b4da16ca66106cf3920d56d25b1c28b4dcf
Rokas <rookas.rudzenskas@gmail.com>
2021-11-30 13:00:22 +0200

    Rokas

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import {StatusBar} from "expo-status-bar";
 import HomeScreen from "./screens/HomeScreen";
 import NewPostScreen from "./screens/NewPostScreen";
+import SignedInStack from "./navigation/navigation";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/*<HomeScreen />*/}
-        <NewPostScreen />
+        <SignedInStack />
       {/*<StatusBar style="auto" />*/}
     </View>
   );
