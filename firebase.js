57498b398c4cf7396a3a10eef588765efbe43cd5
Rokas <rookas.rudzenskas@gmail.com>
2021-11-30 16:14:46 +0200

    Rokas

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import {initializeApp} from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBBfrmCFjhbrDnQy2Nhpva2qbfxqdPIjJk",
+    authDomain: "rookas-ig.firebaseapp.com",
+    projectId: "rookas-ig",
+    storageBucket: "rookas-ig.appspot.com",
+    messagingSenderId: "444280829260",
+    appId: "1:444280829260:web:f2a4692b7d78b91fe54be9",
+    measurementId: "G-DCM72YR60V"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
