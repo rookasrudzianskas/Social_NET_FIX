3aea8cb1c2f3eaac72fa04aeb0976552afad431d
Rokas <rookas.rudzenskas@gmail.com>
2021-11-30 15:19:47 +0200

    Rokas

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
+import {Text, View, StyleSheet, TextInput, Button, TouchableOpacity} from 'react-native';
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 const LoginForm = () => {
     return (
@@ -27,7 +28,12 @@ const LoginForm = () => {
                         secureTextEntry={true}
                     />
                 </View>
-            <Button title={'Login'} />
+            <View style={{alignItems: 'flex-end', marginBottom: 30}}>
+                <Text style={{color: '#6BB0F5'}}>Forgot Password?</Text>
+            </View>
+            <TouchableOpacity activeOpacity={0.5} style={styles.button}>
+                <Text style={{fontWeight: '600', color: 'white'}}>Login</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -42,7 +48,14 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         padding: 12,
         borderWidth: 1,
-        borderColor: '#8d8d8d',
+        borderColor: '#bdbdbd',
         borderRadius: 4,
-    }
+    },
+    button: {
+        backgroundColor: '#0096f6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: 42,
+        borderRadius: 4,
+    },
 });
