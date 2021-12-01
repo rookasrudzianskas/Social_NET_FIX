d5d53bed0949f5f05632a52100a64250335b6fbf
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 09:06:49 +0200

    Rokas

--- navigation/AuthNavigation.js
+++ navigation/AuthNavigation.js
@@ -1,21 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import SignedInStack, {SignedOutStack} from "./navigation";
 import {firebase} from "../firebase";
 
 const AuthNavigation = () => {
 
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const userHandler = (user) => {
+        user ? setCurrentUser(user) : setCurrentUser(null);
+    };
+
+
     useEffect(() => {
         // fire once it loads up
-        const unsub = firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                console.log('user signed in');
-            } else {
-                console.log('user signed out');
-            }
-        });
+        return firebase.auth().onAuthStateChanged(user => userHandler(user));
 
-        return unsub();
     }, []);
 
     return (
